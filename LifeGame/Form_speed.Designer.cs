1efe641f1af93e38c1a99a08176a614f55a88ba0
jprekz <jprekz@users.noreply.github.com>
2013-11-03 18:15:06 +0900

    実行速度の変更機能を追加

---
+++ LifeGame/Form_speed.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace LifeGame
+{
+    partial class Form_speed
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button_apply = new System.Windows.Forms.Button();
+            this.button_cancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "実行速度(ms)";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(94, 48);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(132, 19);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Enter += new System.EventHandler(this.numericUpDown1_Enter);
+            // 
+            // button_apply
+            // 
+            this.button_apply.Location = new System.Drawing.Point(70, 104);
+            this.button_apply.Name = "button_apply";
+            this.button_apply.Size = new System.Drawing.Size(75, 23);
+            this.button_apply.TabIndex = 2;
+            this.button_apply.Text = "適用";
+            this.button_apply.UseVisualStyleBackColor = true;
+            this.button_apply.Click += new System.EventHandler(this.button_apply_Click);
+            // 
+            // button_cancel
+            // 
+            this.button_cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button_cancel.Location = new System.Drawing.Point(151, 104);
+            this.button_cancel.Name = "button_cancel";
+            this.button_cancel.Size = new System.Drawing.Size(75, 23);
+            this.button_cancel.TabIndex = 3;
+            this.button_cancel.Text = "キャンセル";
+            this.button_cancel.UseVisualStyleBackColor = true;
+            this.button_cancel.Click += new System.EventHandler(this.button_cancel_Click);
+            // 
+            // Form_speed
+            // 
+            this.AcceptButton = this.button_apply;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.button_cancel;
+            this.ClientSize = new System.Drawing.Size(238, 139);
+            this.Controls.Add(this.button_cancel);
+            this.Controls.Add(this.button_apply);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form_speed";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "実行速度の変更";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_apply;
+        private System.Windows.Forms.Button button_cancel;
+        public System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
---
+++ LifeGame/Form_speed.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LifeGame
+{
+    public partial class Form_speed : Form
+    {
+        public Form_speed()
+        {
+            InitializeComponent();
+        }
+
+        public bool ApplyFlag = false;
+
+        private void button_apply_Click(object sender, EventArgs e)
+        {
+            ApplyFlag = true;
+            this.Close();
+        }
+
+        private void button_cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void numericUpDown1_Enter(object sender, EventArgs e)
+        {
+            this.numericUpDown1.Select(0, numericUpDown1.Value.ToString().Length);
+        }
+    }
+}
